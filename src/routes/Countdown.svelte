320ba6163ab41915d3fc2afe94cbbd8695dc3d8d
Stefan Nachtrab <stefan.nachtrab@adorsys.com>
2023-03-17 15:51:52 +0100

    time container styling

--- src/routes/Countdown.svelte
+++ src/routes/Countdown.svelte
@@ -1,42 +1,65 @@
 <script xmlns="http://www.w3.org/1999/html">
   import {tweened} from 'svelte/motion';
+  import FaRegPauseCircle from 'svelte-icons/fa/FaRegPauseCircle.svelte';
+  import FaRegPlayCircle from 'svelte-icons/fa/FaRegPlayCircle.svelte';
+  import FaCreativeCommonsSa from 'svelte-icons/fa/FaCreativeCommonsSa.svelte'
 
   let original = 1 * 60; // TYPE NUMBER OF SECONDS HERE
   let timer = tweened(original)
-
-  // ------ dont need to modify code below
-  const countdownInterval = setInterval(() => {
+  let countdownInterval = setInterval(() => {
     if ($timer > 0) {
       $timer--;
     }
   }, 1000);
+  let running = true;
+
+  const startOrPauseTimer = () => {
+    if(running) {
+      clearInterval(countdownInterval);
+      running = false;
+    } else {
+      countdownInterval = setInterval(() => {
+        if ($timer > 0) {
+          $timer--;
+        }
+      }, 1000);
+      running = true;
+    }
+
+  }
+
+  const stopTimer = () => {
+    clearInterval(countdownInterval);
+    running = false;
+  }
+
 
   $: minutes = Math.floor($timer / 60);
   $: seconds = Math.floor($timer - minutes * 60)
 </script>
 
 <main>
-
-  {#if timer < 1}
-    <Typewriter loop>
-      <h1>Starting soon....</h1>
-    </Typewriter>
-  {:else}
     <div class="container">
       <div class="timercontainer">
         <div class="buttongroup">
-          <input type="button" class="button" value="start"/>
-          <input type="button" class="button" value="stop"/>
+
+          {#if running}
+            <span on:click="{startOrPauseTimer}"><FaRegPauseCircle /></span>
+          {:else}
+            <span on:click="{startOrPauseTimer}"><FaRegPlayCircle /></span>
+          {/if}
+          <span on:click="{stopTimer}"><FaCreativeCommonsSa /></span>
         </div>
-        <input type="number" class="mins" value="{('0'+minutes).slice(-2)}"/>
-        <input type="number" class="secs" value="{('0'+seconds).slice(-2)}"/>
-      </div>
+        <div class="timer">
+          <input type="number" class="mins" value="{('0'+minutes).slice(-2)}" on:click="{() => clearInterval(countdownInterval)}"/>
+          <span class="divider">:</span>
+          <input type="number" class="secs" value="{('0'+seconds).slice(-2)}" on:click="{() => clearInterval(countdownInterval)}"/>
+        </div>
+        </div>
       <div class="progresscontainer">
         <progress value={$timer/original} class="progress"></progress>
       </div>
     </div>
-
-  {/if}
   <!-- 	feel free to modify this text!! -->
 </main>
 
@@ -46,12 +69,12 @@
   }
   .buttongroup {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    gap: 0.5rem;
     align-self: center;
-    margin-right: 3rem;
   }
-  .button {
-    margin: 0.2rem auto;
+  .buttongroup > span {
+    height: 30px;
   }
   .timercontainer {
     margin-top: 2rem;
@@ -67,20 +90,25 @@
     border: none;
     text-align: center;
     font-size: 4rem;
-    width: 25%;
-    min-width: 90px;
-    max-width: 16rem;
   }
   .secs {
     background: none;
     border: none;
     text-align: center;
-    font-size: 3rem;
-    width: 20%;
-    max-width: 10rem;
-    min-width: 90px;
+    font-size:3rem;
     padding-bottom: 5px;
   }
+  .divider {
+    font-size: 2rem;
+    padding-bottom: 14px;
+  }
+
+  .timer {
+    display: flex;
+
+    width: 50%;
+  }
+
   .progresscontainer {
     width: 100%;
     display: flex;
